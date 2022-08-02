c28e974c76046f61468ba0bd6154a9be164cbc75
Lucas <lucasgrafimar@gmail.com>
2022-08-02 16:12:46 -0300

    aa

--- core.js
+++ core.js
@@ -5,6 +5,17 @@ const height = size * rows;
 const width = size * columns;
 const gravity = 100;
 
+const colors = [
+  "#00f",
+  "#f00",
+  "#0f0",
+  "#0ff",
+  "#f51",
+  "#25f",
+  "#f25",
+  "#453123",
+];
+
 class Piece {
   constructor(x, y, type) {
     this.y = y;
@@ -12,99 +23,91 @@ class Piece {
     this.type = type;
     if (type == 1) {
       this.shape = [
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 1],
+        [0, type, 0],
+        [0, type, type],
+        [0, 0, type],
       ];
       this.size = 3;
-      this.color = "#00f";
     } else if (type == 2) {
       this.shape = [
-        [1, 1, 1],
-        [0, 1, 0],
         [0, 0, 0],
+        [type, type, type],
+        [0, type, 0],
       ];
       this.size = 3;
-      this.color = "#f00";
     } else if (type == 3) {
       this.shape = [
-        [1, 0, 0],
-        [1, 1, 1],
         [0, 0, 0],
+        [type, 0, 0],
+        [type, type, type],
       ];
       this.size = 3;
-      this.color = "#0f0";
     } else if (type == 4) {
       this.shape = [
-        [1, 1],
-        [1, 1],
+        [type, type],
+        [type, type],
       ];
       this.size = 2;
-      this.color = "#0ff";
     } else if (type == 5) {
       this.shape = [
-        [0, 0, 1],
-        [1, 1, 1],
         [0, 0, 0],
+        [0, 0, type],
+        [type, type, type],
       ];
       this.size = 3;
-      this.color = "#f51";
     } else if (type == 6) {
       this.shape = [
-        [1, 1, 0],
-        [0, 1, 1],
         [0, 0, 0],
+        [type, type, 0],
+        [0, type, type],
       ];
       this.size = 3;
-      this.color = "#25f";
     } else if (type == 7) {
       this.shape = [
-        [0, 1, 1],
-        [1, 1, 0],
         [0, 0, 0],
+        [0, type, type],
+        [type, type, 0],
       ];
       this.size = 3;
-      this.color = "#f25";
     } else if (type == 8) {
       this.shape = [
-        [1, 1, 1, 1],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
         [0, 0, 0, 0],
+        [type, type, type, type],
       ];
       this.size = 4;
-      this.color = "#453123";
     }
   }
 
   rotate(gameScreen) {
     let copy = [...Array(this.size)].map(() => [...Array(this.size)]);
 
-    // fez a copy girando a piece
     for (let i = 0; i < this.size; i++)
       for (let j = 0; j < this.size; j++)
-        copy[this.size - 1 - j][i] = this.shape[j][i];
+        copy[j][this.size - 1 - i] = this.shape[i][j];
 
-    // verificar se copy colide com algo
-    // for (let i = 0; i < this.size; i++)
-    //   for (let j = 0; j < this.size; j++) {
-    //     if (copy[i][j] != 0 && this.y + i < rows) return 0;
-    //     if (copy[i][j] != 0 && gameScreen.grade[this.y + i][this.x + j] != 0)
-    //       return 0;
-    //   }
-    // copiar a copy para o self.grade
+    for (let i = 0; i < this.size; i++)
+      for (let j = 0; j < this.size; j++)
+        if (
+          copy[i][j] != 0 &&
+          (this.y + j >= rows || gameScreen.grid[this.y + i][this.x + j] != 0)
+        )
+          return 0;
+
     for (let i = 0; i < this.size; i++)
       for (let j = 0; j < this.size; j++) this.shape[i][j] = copy[i][j];
 
     return 1;
   }
+
   down(gameScreen) {
     for (let i = 0; i < this.size; i++)
       for (let j = 0; j < this.size; j++) {
         if (
           this.shape[i][j] != 0 &&
           this.y + i + 1 < rows &&
-          gameScreen.grade[this.y + i + 1][this.x + j] != 0
+          gameScreen.grid[this.y + i + 1][this.x + j] != 0
         )
           return 0;
         if (this.shape[i][j] != 0 && this.y + i + 1 >= rows) return 0;
@@ -116,7 +119,8 @@ class Piece {
     for (let i = 0; i < this.size; i++)
       for (let j = 0; j < this.size; j++) {
         if (this.shape[i][j] != 0 && this.x + 1 + j >= columns) return 0;
-        if (gameScreen.grade[this.y][this.x] * this.shape[i][j] != 0) return 0;
+        if (gameScreen.grid[this.y][this.x] != 0 && this.shape[i][j] != 0)
+          return 0;
       }
 
     this.x++;
@@ -125,7 +129,8 @@ class Piece {
   left(gameScreen) {
     for (let i = 0; i < this.size; i++)
       for (let j = 0; j < this.size; j++) {
-        if (gameScreen.grade[this.y][this.x] * this.shape[i][j] != 0) return 0;
+        if (gameScreen.grid[this.y][this.x] != 0 && this.shape[i][j] != 0)
+          return 0;
         if (this.shape[i][j] != 0 && this.x - 1 + j < 0) return 0;
       }
 
@@ -139,6 +144,13 @@ class Piece {
         if (this.shape[i][j] != 0) {
           ctx.fillStyle = "#000";
           ctx.fillRect((this.x + j) * 20, (this.y + i) * 20, 20, 20);
+          ctx.fillStyle = colors[this.shape[i][j] - 1];
+          ctx.fillRect(
+            (this.x + j) * 20 + 1,
+            (this.y + i) * 20 + 1,
+            20 - 1,
+            20 - 1
+          );
         }
       }
     }
@@ -147,10 +159,10 @@ class Piece {
 
 class GameScreen {
   constructor() {
-    this.grade = [...Array(rows)].map(() => [...Array(columns)]);
+    this.grid = [...Array(rows)].map(() => [...Array(columns)]);
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < columns; j++) {
-        this.grade[i][j] = 0;
+        this.grid[i][j] = 0;
       }
     }
   }
@@ -158,7 +170,7 @@ class GameScreen {
     let removeLines = [];
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < columns; j++) {
-        if (this.grade[i][j] == 0) break;
+        if (this.grid[i][j] == 0) break;
         if (j == columns - 1) removeLines.push(i);
       }
     }
@@ -168,11 +180,11 @@ class GameScreen {
   dropLine(removeLines) {
     for (let i = 0; i < removeLines.length; i++) {
       for (let j = 0; j < columns; j++) {
-        this.grade[removeLines[i]][j] = 0;
+        this.grid[removeLines[i]][j] = 0;
       }
       for (let z = rows - 1; z > 0; z--) {
         for (let j = 0; j < columns; j++) {
-          if (z - 1 >= 0) this.grade[z][j] = this.grade[z - 1][j];
+          if (z - 1 >= 0) this.grid[z][j] = this.grid[z - 1][j];
         }
       }
     }
@@ -182,20 +194,24 @@ class GameScreen {
     for (let i = 0; i < piece.size; i++)
       for (let j = 0; j < piece.size; j++) {
         if (piece.shape[i][j] != 0) {
-          this.grade[piece.y + i][piece.x + j] = piece.shape[i][j];
+          this.grid[piece.y + i][piece.x + j] = piece.shape[i][j];
         }
       }
   }
 
-  draw(ctx) {
+  draw(ctx, piece) {
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < columns; j++) {
-        if (this.grade[i][j] != 0) {
-          ctx.fillStyle = "#0f0";
+        if (this.grid[i][j] != 0) {
+          ctx.fillStyle = "#000";
           ctx.fillRect(j * 20, i * 20, 20, 20);
+          ctx.fillStyle = colors[this.grid[i][j] - 1];
+          ctx.fillRect(j * 20 + 1, i * 20 + 1, 20 - 1, 20 - 1);
         } else {
-          ctx.fillStyle = "#fff";
+          ctx.fillStyle = "#000";
           ctx.fillRect(j * 20, i * 20, 20, 20);
+          ctx.fillStyle = "#fff";
+          ctx.fillRect(j * 20 + 1, i * 20 + 1, 20 - 1, 20 - 1);
         }
       }
     }
@@ -203,19 +219,16 @@ class GameScreen {
 }
 
 const randomPiece = () => {
-  return 4;
+  // return 4;
   return Math.floor(Math.random() * 8) + 1;
 };
 
 window.addEventListener("keydown", (event) => {
   if (event.keyCode == 37) {
-    console.log("left");
     piece.left(gameScreen);
   } else if (event.keyCode == 38) {
-    console.log("Rotation");
     piece.rotate(gameScreen);
   } else if (event.keyCode == 39) {
-    console.log("Direita");
     piece.right(gameScreen);
   }
   // else if (event.keyCode == 40) currentShape.moveBottom();
@@ -228,7 +241,7 @@ const drawBorder = () => {
 
 const draw = () => {
   context.clearRect(0, 0, width, height);
-  gameScreen.draw(context);
+  gameScreen.draw(context, piece);
   piece.draw(context);
   drawBorder();
 };
@@ -249,7 +262,7 @@ const run = async () => {
       for (let j = 0; j < piece.size; j++)
         if (
           piece.shape[i][j] != 0 &&
-          gameScreen.grade[piece.y + i][piece.x + j] != 0
+          gameScreen.grid[piece.y + i][piece.x + j] != 0
         ) {
           console.log("Game Over");
           return;
